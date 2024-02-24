e75a4e69d03dd2c8424c9ce5ccc35bc94db04e0b
jai prashanth <Jaiprashanth003@gmail.com>
2024-02-24 19:03:08 +0530

    first commit

---
+++ app/actions.tsx
@@ -0,0 +1,12 @@
+'use server'
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
+export const fetchanime = async(page:number)=>{
+    const respone = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`)
+    const data = await respone.json();
+
+    return data.map((item:AnimeProp,index: number)=>(
+        <AnimeCard key={item.id} anime={item} index = {index}/>
+    ))
+}
